4f017fad56842d58d71bac97e51e62ba8ca83a4e
Kevin Harrison <kharrison7@elon.edu>
2017-07-26 14:19:38 -0400

    This alters the limiter based on fib number size rather than i value.:

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let fib = []; // Initialize array!
 
 fib[0] = 0;
 fib[1] = 1;
-for(i=2; i<=33; i++)
+for(i=2; fib[ fib.length - 1 ] < 4e+6; i++)
 {
     // Next fibonacci number = previous + one before previous
     // Translated to JavaScript:
