cf35f2f0631fa6e818b11f7c99f35486bc27ee7e
Kevin Harrison <kharrison7@elon.edu>
2017-07-26 16:07:01 -0400

    Adds FizzBuzz button and JS.

--- main.js
+++ main.js
@@ -63,3 +63,49 @@ function solution()
 }
 
 console.log(solution())
+
+
+//This is for fizzBuzz
+function fizzBuzz(){
+
+//Writes a an array up to 100.
+let i;
+let fib = []; // Initialize array!
+
+fib[0] = 0;
+fib[1] = 1;
+for(i=1; fib[ fib.length - 1 ] < 100; i++)
+{
+    // Next fibonacci number = previous + one before previous
+    // Translated to JavaScript:
+    fib[i] = i;
+    //console.log(fib[i]);
+}
+
+fib.splice(0, 1);
+console.log(fib[0]);
+console.log(fib[1]);
+console.log(fib[2]);
+console.log(fib[3]);
+
+let a = 0;
+//fib = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+for(a=0; a<(fib.length-1); a++){
+  if(fib[a]%3 === 0 && fib[a]%5 === 0){
+    console.log('FizzBuzz');
+  }
+    else if (fib[a]%3 === 0){
+      console.log('Fizz');
+    }
+    else if (fib[a]%5 === 0){
+      console.log('Buzz');
+    }
+    else{
+      console.log(fib[a]);
+    }
+}
+ console.log(100);
+
+}
